8ca238f9aeb25c93b46834a89b1a37d65a126d7e
tellydelly <martinwilson450@gmail.com>
2022-06-30 16:54:00 -0700

    created state model

--- p4_backend_app/models.py
+++ p4_backend_app/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=35)
+    abbreviation = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.abbreviation
