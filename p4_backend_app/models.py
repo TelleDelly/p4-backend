7886771fa203c6a0e7ecf1071e25e2479ca3b94c
tellydelly <martinwilson450@gmail.com>
2022-07-02 10:01:11 -0700

    created seeding file and made migrations

--- p4_backend_app/models.py
+++ p4_backend_app/models.py
@@ -16,7 +16,7 @@ class Gestational_Limits(models.Model):
     banned_after_weeks_since_LMP = models.IntegerField()
     exception_life = models.BooleanField()
     exception_health = models.CharField(max_length=23)
-    exception_fetal = models.CharField(23)
+    exception_fetal = models.CharField(max_length=23)
     exception_rape_or_incest = models.BooleanField()
     state = models.ForeignKey(State, on_delete=models.CASCADE, related_name='gestational_limits')
 
