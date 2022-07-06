04603783dce0241f04af52f9eb60b7a875fe90d5
tellydelly <martinwilson450@gmail.com>
2022-07-06 14:46:21 -0700

    added state policy controller

--- controllers/statePolicyController.js
+++ controllers/statePolicyController.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const {Router} = require('express')
-
-
 const statePolicyRouter = express.Router()
+
+const StatePolicy = require('../models/stateModel')
+
+statePolicyRouter.get('/:stateID', (req, res) => {
+    StatePolicy.findById(req.params.stateID)
+    .then((policies) => res.send(policies))
+    .catch(console.error)
+})
