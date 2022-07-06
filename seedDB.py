f2e49c6babf7840b2533487fb7729038c71b3166
tellydelly <martinwilson450@gmail.com>
2022-07-05 17:47:56 -0700

    http request works in custom command

--- seedDB.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-import json
-
-gestation_limit_url = 'https://api.abortionpolicyapi.com/v1/gestational_limits/states'
-headers = { 'token': os.environ['APIKEY']}
-
-req = requests.get(gestation_limit_url, headers=headers)
