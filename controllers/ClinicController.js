2302363134c51d0020ef416664b63e5fe193f1f6
tellydelly <martinwilson450@gmail.com>
2022-07-16 11:50:20 -0700

    Created routes for clinic get

--- controllers/ClinicController.js
+++ controllers/ClinicController.js
@@ -3,12 +3,16 @@ const express = require('express')
 const {Router} = require('express')
 const ClinicRouter = express.Router()
 
-const Clinics = require('../models/reviewModel')
+const Clinics = require('../models/clinicModel')
 
-ClinicRouter.get('/:stateName', (req, res) => {
-    Clinics.find({state: req.params.stateName})
+ClinicRouter.get('/state/:stateName', (req, res) => {
+    Clinics.find({'address.state': req.params.stateName})
     .then((results) => res.json(results))
     .catch(console.error)
+})
+
+ClinicRouter.get('/test', (req, res) => {
+    console.log('test')
 })
 
 module.exports = ClinicRouter
