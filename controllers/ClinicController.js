13554733cc0346c02ee639bfab2afb5bf2a4d4d8
tellydelly <martinwilson450@gmail.com>
2022-07-16 10:54:03 -0700

    created clinic controller

---
+++ controllers/ClinicController.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const {Router} = require('express')
+const ClinicRouter = express.Router()
+
+const Clinics = require('../models/reviewModel')
+
+ClinicRouter.get('/:stateName', (res, res) => {
+    let stateName = req.params.stateName
+    Clinics.find({state: staetName})
+    .then((results) => res.json(results))
+    .catch(console.error)
+})
+
+module.exports = ReviewRouter
