aed257378b7a1c18ab4b3b04555397234bb1a085
TelleDelly <martinwilson450@gmail.com>
2022-09-19 05:53:12 -0700

    formatting

--- controllers/ClinicController.js
+++ controllers/ClinicController.js
@@ -1,29 +1,28 @@
-const express = require('express')
+const express = require("express");
 
-const {Router} = require('express')
-const ClinicRouter = express.Router()
+const { Router } = require("express");
+const ClinicRouter = express.Router();
 
-const Clinics = require('../models/clinicModel')
+const Clinics = require("../models/clinicModel");
 
-ClinicRouter.get('/id/:ID', async (req, res) => {
-    try{
-        const clinic = await Clinics.findById(req.params.ID)
-        res.json(clinic)
-    } 
-    catch(error){
-        console.error
-    }
-    
-})
+ClinicRouter.get("/id/:ID", async (req, res) => {
+  try {
+    const clinic = await Clinics.findById(req.params.ID);
+    res.json(clinic);
+  } catch (error) {
+    console.error;
+  }
+});
 
-ClinicRouter.get('/state/:stateName', async (req, res) => {
-    try{
-        const clinics = await Clinics.find({'address.state': req.params.stateName})
-    res.json(clinics)
-    }
-    catch(error){
-        console.error
-    }
-})
+ClinicRouter.get("/state/:stateName", async (req, res) => {
+  try {
+    const clinics = await Clinics.find({
+      "address.state": req.params.stateName,
+    });
+    res.json(clinics);
+  } catch (error) {
+    console.error;
+  }
+});
 
-module.exports = ClinicRouter
+module.exports = ClinicRouter;
