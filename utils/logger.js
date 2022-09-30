657480eaa1f5a0037e87db37f8dfa3691c8e3ef2
TelleDelly <martinwilson450@gmail.com>
2022-09-29 18:06:24 -0700

    creating log clear scripts and updating bugin logger printout

--- utils/logger.js
+++ utils/logger.js
@@ -28,7 +28,7 @@ const format = winston.format.combine(
     winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss:ms'}),
     winston.format.colorize({all: true}),
     winston.format.printf(
-        (info) => `${info.timestamp} ${info.levl} ${info.message}`,
+        (info) => `${info.timestamp} ${info.level} ${info.message}`,
     ),
 )
 
