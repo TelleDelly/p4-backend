f481dfb40f0fbafb41f7c59b7148b5eaff1992e4
TelleDelly <martinwilson450@gmail.com>
2022-09-30 15:25:22 -0700

    attempting at better error handling

--- controllers/ClinicController.js
+++ controllers/ClinicController.js
@@ -1,27 +1,27 @@
 const express = require("express");
-
 const { Router } = require("express");
 const ClinicRouter = express.Router();
 
 const Clinics = require("../models/clinicModel");
+const CustomError = require("../utils/CustomError");
 
-ClinicRouter.get("/id/:ID", async (req, res) => {
+ClinicRouter.get("/id/:ID", async (req, res, next) => {
   try {
     const clinic = await Clinics.findById(req.params.ID);
-    res.json(clinic);
+    res.status(200).json(clinic);
   } catch (error) {
-    console.error;
+    next(new CustomError('No clinic found', 404))
   }
 });
 
-ClinicRouter.get("/state/:stateName", async (req, res) => {
+ClinicRouter.get("/state/:stateName", async (req, res, next) => {
   try {
     const clinics = await Clinics.find({
       "address.state": req.params.stateName,
     });
-    res.json(clinics);
+    res.status(200).json(clinics);
   } catch (error) {
-    console.error;
+    next(new CustomError('No clinic found', 404))
   }
 });
 
