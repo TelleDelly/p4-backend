d96bb5f170634e2cdee72f08e1165fb14838763f
TelleDelly <martinwilson450@gmail.com>
2023-02-04 18:48:42 -0800

    group model

---
+++ models/GroupModel.js
@@ -0,0 +1,63 @@
+const {Schema} = require('mongoose')
+const mongoose =require('../db/Connection')
+
+const GroupSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            maxlength: 180, 
+        },
+        areaOfOperation: {
+            city: {
+                cityName: String,
+                cityRegion: String,
+            },
+            state: {
+                stateName: String,
+                stateRegion: String,
+            },
+            region: {
+                regionName: String,
+                cities: Array,
+                states: Array
+            }
+        },
+        resourceType: {
+            type: Array,
+            required: true
+        },
+        contactInfo: {
+            websiteURL: URL,
+            phone: String,
+            email: String,
+            socialMedia: Array,
+            pointOfContact: String,
+        },
+        outreach:{
+            type: Array
+        },
+        connections: {
+            type: Array
+        },
+        services: {
+            type: Array
+        },
+        misc: {
+            type: Array
+        },
+        about: {
+            type: String,
+            required: true,
+            maxlength: 800
+        },
+        shortSummary: {
+            type: String,
+            required: true,
+            maxlength: 1000
+        }
+    }
+)
+
+const Group = mongoose.model('Group', GroupSchema)
+module.exports = Group
